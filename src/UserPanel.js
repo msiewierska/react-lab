fa6642582f49985460ba3ee555343967886900f0
msiewierska <marta.siewierska-stanczak@creativestyle.pl>
2024-05-18 10:52:31 +0200

    Add meetings

--- src/UserPanel.js
+++ src/UserPanel.js
@@ -1,8 +1,10 @@
 import {useState} from "react";
+import MeetingsPage from "./meeting/MeetingsPage";
 
 export default function UserPanel(props) {
 
     return <div>
         <h2>Jesteś zalogowany</h2>
+        <MeetingsPage />
     </div>;
 }
